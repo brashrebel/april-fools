40d387b3be330ee4172d418c1fcd9cfcb1a9295e
brashrebel <kyle@realbigmarketing.com>
2014-09-09 07:55:32 -0400

    Added a readme.md Formatted the code Made some parts more generic

--- april-fools.php
+++ april-fools.php
@@ -1,26 +1,30 @@
 <?php
 /*
 Plugin Name: April Fools
-Description: Just some holiday fun.
+Description: This plugin turns the WordPress admin bar pink for only certain users.
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
-Version: 1.0
+Version: 1.1
 License: GPL2
 */
-add_action('admin_print_styles', 'kjm_prank');
-add_action('wp_print_styles', 'kjm_prank');
-function kjm_prank() {
-	$user_id = get_current_user_id();
-	$april_one = '1 April';
-	$current_date = date('j F');
-	if ($current_date == $april_one) { 
-		if ($user_id == 4 || $user_id == 6 || $user_id == 91 || $user_id == 103) { ?>
-		<style type="text/css">
-		#wpcontent #wpadminbar {
-			background: #DD30E4;
+add_action( 'admin_print_styles', 'af_prank' );
+add_action( 'wp_print_styles', 'af_prank' );
+function af_prank() {
+	$user_id      = get_current_user_id();
+	$victim      = 1; // CHANGE THIS TO BE USER ID OF YOUR VICTIM
+	$april_one    = '1 April';
+	$current_date = date( 'j F' );
+	if ( $current_date == $april_one ) {
+		if ( $user_id == $victim ) {
+			?>
+			<style type="text/css">
+				#wpcontent #wpadminbar {
+					background: #DD30E4;
+				}
+			</style>
+		<?php
+		} else {
 		}
-		</style>
-			<?php } else { }
-		} else { }
+	} else {
+	}
 }
-?>
